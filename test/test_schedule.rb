a79d7d6c30aa784362dac06af45c0cb77b251de9
Alex Tom <alexgtom@berkeley.edu>
2013-01-10 20:04:08 -0800

    Added test cases

---
+++ test/test_schedule.rb
@@ -0,0 +1,24 @@
+require 'testing_env'
+
+class ScheduleTests < Test::Unit::TestCase
+  def test_schedule_url
+    assert_equal("#{SCHEDULE_URL}", schedule_url())
+    assert_equal("#{SCHEDULE_URL}p_term=FL", schedule_url(:term => "FL"))
+    assert_equal("#{SCHEDULE_URL}p_term=FL&p_dept=CHEM", 
+                 schedule_url(:term => "FL", :dept => "CHEM"))
+  end
+
+end
+
+class HtmlTokenizerTests < Test::Unit::TestCase
+  def test_tokenize_page
+    assert_equal(["<table>", "</table>"], 
+                 HtmlTokenizer.new("<table></table>").each{|v| v})
+    assert_equal(["<table>", "hello", "</table>"], 
+                 HtmlTokenizer.new("<table>hello</table>").each{|v| v})
+    assert_equal(["hello"], 
+                 HtmlTokenizer.new("hello").each{|v| v})
+    assert_equal([], 
+                 HtmlTokenizer.new("").each{|v| v})
+  end
+end
